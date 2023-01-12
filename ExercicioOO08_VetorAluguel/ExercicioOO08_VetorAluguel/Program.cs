a8a6d31e64406045f81a260bc8e28ac5d5a38147
Talison Moura <111541453+TalisonMoura@users.noreply.github.com>
2023-01-12 17:23:33 -0300

    Add files via upload

---
+++ ExercicioOO08_VetorAluguel/ExercicioOO08_VetorAluguel/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+
+namespace ExercicioOO08_VetorAluguel {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Estudante[] vect = new Estudante[10];
+
+            Console.Write("How many rooms will be rented? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine($"Rent #{i}:");
+                Console.Write("Name: ");
+                String Name = Console.ReadLine();
+                Console.Write("Email: ");
+                String Email = Console.ReadLine();
+                Console.Write("Room: ");
+                int Room = int.Parse(Console.ReadLine());
+                vect[Room] = new Estudante(Name, Email);
+                Console.WriteLine();
+            }
+            Console.WriteLine("Busy Rooms: ");
+            for (int i = 0; i < 10; i++) {
+                if (vect[i] != null) {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+        }
+    }
+}

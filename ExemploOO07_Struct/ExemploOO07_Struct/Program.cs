d97926fecb24a0c9415427fa055a6e12fabdd4d9
Talison Moura <111541453+TalisonMoura@users.noreply.github.com>
2023-01-12 17:26:35 -0300

    Add files via upload

---
+++ ExemploOO07_Struct/ExemploOO07_Struct/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ExemploOO07_Struct {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Point p;
+
+            p.x = 10; 
+            p.y = 20;
+
+            Console.WriteLine(p);
+
+        }
+    }
+}

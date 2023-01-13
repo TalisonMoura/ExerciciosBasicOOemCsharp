e4c2401510d1dc2a7d262156f33c298708c6f6aa
Talison Moura <111541453+TalisonMoura@users.noreply.github.com>
2023-01-12 23:10:26 -0300

    Add files via upload

---
+++ ExemploOO10_ModificadorParamsRefOut/ExemploOO10_ModificadorParamsRefOut/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ExemploOO10_ModificadorParamsRefOut {
+    internal class Program {
+        static void Main(string[] args) {
+            /*
+            int a = 10;
+            Calculator.Triple(ref a);
+            Console.WriteLine(a);
+            */
+
+            int a = 10;
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
+        }
+    }
+}
---
+++ ExemploOO11_LacoForEach/ExemploOO11_LacoForEach/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ExemploOO11_LacoForEach {
+    internal class Program {
+        static void Main(string[] args) {
+
+            string[] vect = new string[] { "Maria", "Ana", "Talison" };
+
+            for (int i = 0; i < vect.Length; i++) {
+                Console.WriteLine(vect[i]);
+            }
+            Console.WriteLine("-------------------------------");
+            foreach(string obj in vect) {       
+                Console.WriteLine(obj);                                     
+            }
+        }
+    }
+}
